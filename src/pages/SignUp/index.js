0d0767e43ce8f03c4897072c37eb10e3ce87b069
GalloGabriel <gabriel.gallo2242@gmail.com>
2021-08-03 19:35:05 -0300

    criando tela de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 
 function SignUp() {
+
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault(); //para não atualizar a página
+    alert('CLICASTES')
+  }
+
   return (
-    <div>
-      <h1>Tela de Cadastro</h1>
+    <div className="container-center">
+
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="logotipo da empresa"/>
+        </div>
+
+        <form onSubmit={ handleSubmit }>
+          <h1>Cadastre-se</h1>
+
+          <input type="text"
+          placeholder="Digite seu nome"
+          value={nome}
+          onChange={ (e) => setNome(e.target.value) }/>
+
+          <input type="text" 
+          placeholder="digite seu email" 
+          value={email}
+          onChange={ (e) => setEmail(e.target.value)}
+           />
+
+          <input type="password" 
+          placeholder="digite sua senha"
+          value={password}
+          onChange={ (e) => setPassword(e.target.value) }
+           />
+
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já possui uma conta? Entre</Link>
+      </div>
+
     </div>
   );
 }
