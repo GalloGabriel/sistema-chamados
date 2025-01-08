0b1fbb82840b901ada120da7f6fae5028c9f69eb
GalloGabriel <gabriel.gallo2242@gmail.com>
2025-01-08 17:17:25 -0300

    atualizando import do firebase

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -1,7 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import 'firebase/storage';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 let firebaseConfig = {
   apiKey: "AIzaSyAnTGJtqbNASdPg6mc69hUli--bMxLbGN4",
